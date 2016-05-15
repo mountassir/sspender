fdf20592b03f60dd0687bd888205333c74a2c0d6
mountassir <mountassirbillah1@gmail.com>
2016-05-15 01:10:08 +0100

    Update comments

--- src/Disk.h
+++ src/Disk.h
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2016 Mountassir El Hafi, (mountassirbillah1@gmail.com)
+ *
+ * Writer.cpp: Part of sspender
+ *
+ * sspender is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * sspender is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with sspender.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef DISK_H_
 #define DISK_H_
 
